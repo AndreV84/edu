91c9b1dec5bc7c6d86016fc420c41ae174725055
Andrey Volodin <andrevolodin@gmail.com>
2019-06-20 04:20:36 +0000

    Update salt.sh

--- salt.sh
+++ salt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo launchctl limit maxfiles 4096 8192
-brew install saltstack
+sudo mkdir /etc/salt
+#brew install saltstack
 curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
 sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
 sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist
