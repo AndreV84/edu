7c22869ceb6b0c593947b93dc27e87a17eec7b98
Andrey Volodin <andrevolodin@gmail.com>
2019-06-20 05:29:56 +0000

    Update salt.sh

--- salt.sh
+++ salt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo type ip address of your master server, please, e.g. 10.0.0.1  and press ENTER && read master && echo master: $master >minion && sudo mkdir /etc/salt
-sudo mv minion /etc/salt/
+#echo type ip address of your master server, please, e.g. 10.0.0.1  and press ENTER && read master && echo master: $master >minion && sudo mkdir /etc/salt
+#sudo mv minion /etc/salt/
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo launchctl limit maxfiles 4096 8192
-#echo master : 10.0.0.2 > minion
+echo master : 10.0.0.2 > minion
 brew install saltstack
 curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
 sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
