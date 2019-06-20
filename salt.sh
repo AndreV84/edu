19775be63520eef2e860bb3d475affc3e90041df
Andrey Volodin <andrevolodin@gmail.com>
2019-06-20 05:32:38 +0000

    Update salt.sh

--- salt.sh
+++ salt.sh
@@ -2,7 +2,7 @@
 echo Welcome to the Autoinstall by AndreV84 !
 echo The installer will put a placeholder for the ip address of the master server to the file /etc/salt/minion
 echo update the value with your ip address of the master, please
-ip address of your master server, please, e.g. 10.0.0.1  and press ENTER && read master && echo master: $master >minion && sudo mkdir /etc/salt
+#ip address of your master server, please, e.g. 10.0.0.1  and press ENTER && read master && echo master: $master >minion && sudo mkdir /etc/salt
 # sudo mv minion /etc/salt/
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo launchctl limit maxfiles 4096 8192
