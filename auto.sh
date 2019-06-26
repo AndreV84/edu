601b6e4b52c0adb477032f40681246dc39de8e5e
Andrey Volodin <andrevolodin@gmail.com>
2019-06-26 11:41:00 +0000

    Update auto.sh
    refined

--- auto.sh
+++ auto.sh
@@ -4,20 +4,10 @@ sudo mkdir /etc/salt
 sudo mv minion /etc/salt/
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo launchctl limit maxfiles 4096 8192
-echo master : 10.0.0.2 > minion
 brew install saltstack
 curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
 sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
 sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist
 sudo salt -G 'os:macos' cmd.run "curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.mini$
-sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && 
-sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist"/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo launchctl limit maxfiles 4096 8192
-echo master : 10.0.0.2 > minion
-brew install saltstack
-curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
-sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
-sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist
-sudo salt -G 'os:macos' cmd.run "curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.minion.plist && 
 sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && 
 sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist"
