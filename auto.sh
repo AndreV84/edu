b439eeb6a43efe8c21cb623495eac09f8be24faf
Andrey Volodin <andrevolodin@gmail.com>
2019-06-26 10:38:55 +0000

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,5 +1,17 @@
-echo type ip address of your master server, please, e.g. 10.0.0.1  and press ENTER && read master && wait && echo master: $master >minion
+#!/bin/bash
+echo enter master ip address in the prompt below
+read -p master
+echo master: $master > minion
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo launchctl limit maxfiles 4096 8192
+echo master : 10.0.0.2 > minion
+brew install saltstack
+curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
+sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && \
+sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist
+sudo salt -G 'os:macos' cmd.run "curl -L https://raw.githubusercontent.com/saltstack/salt/develop/pkg/darwin/com.saltstack.salt.minion.plist | sudo tee cp /Library/LaunchDaemons/com.saltstack.salt.mini$
+sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist && 
+sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist"/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo launchctl limit maxfiles 4096 8192
 echo master : 10.0.0.2 > minion
 brew install saltstack
