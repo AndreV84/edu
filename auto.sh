c5ef1b96905d0f591ba5d22fe34720b228dd43af
Andrey Volodin <andrevolodin@gmail.com>
2019-06-26 17:11:51 +0000

    Update auto.sh
    added:
    sudo chown -R $(whoami) /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew/locks /usr/local/Homebrew/

--- auto.sh
+++ auto.sh
@@ -2,6 +2,7 @@ echo type ip address of your master server, please, e.g. 10.0.0.1  and press ENT
 wait
 sudo mkdir /etc/salt
 sudo mv minion /etc/salt/
+sudo chown -R $(whoami) /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew/locks /usr/local/Homebrew/
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo launchctl limit maxfiles 4096 8192
 brew install saltstack
